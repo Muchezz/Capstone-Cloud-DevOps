a990c5b8fc4aec12ef863d2377e4c4450ed0b040
Mucheru <mucheru@outlook.com>
2022-01-31 16:10:17 +0300

    Update Readme

--- app/app.py
+++ app/app.py
@@ -34,4 +34,4 @@ app=Flask(__name__)
 def home():
     return render_template("index.html",table=cdf, cmap=html_map,pairs=pairs)
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0" port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
